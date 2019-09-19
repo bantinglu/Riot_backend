03a943d160ec1137c31e0e5327f98a3e02d941b7
Banting Lu <banting.lu.0514@gmail.com>
2019-09-19 16:08:45 -0400

    going to try to store matches in mongodb, init, wil branch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Kayn, REGIONS } = require('kayn');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const searchRouter = require('./routes/search');
 
@@ -11,6 +12,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () =>{
+    console.log("MongoDB connection success");
+});
 
 app.listen(port, () =>{
     console.log(`Server running on port: ${port}`);
