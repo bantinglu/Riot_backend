2588a56cc9472424fc0081dcc61ab1078e9ac550
Banting Lu <banting.lu.0514@gmail.com>
2019-12-05 00:25:19 -0500

    refactored promise chain

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Kayn, REGIONS } = require('kayn');
 const kayn = Kayn(process.env.API_KEY)();
+const _ = require('lodash');
 
 var jsonQuery = require('json-query')
 var items = require('./../item.json');
@@ -8,7 +9,7 @@ var runes = require('./../runesReforged.json');
 var summoners = require('./../summoner.json');
 var champs = require('./../champion.json');
  
-router.route('/:name').get((req, res) => {
+/*router.route('/:name').get((req, res) => {
     
     var matchesReturns = [];
 
@@ -19,7 +20,7 @@ router.route('/:name').get((req, res) => {
             .then (matchlist=> {
                 var matchPromises = [];
                 //FOR BREVITY - determines how many matches to search
-                matchlist.matches.slice(0,5).map( x => {
+                matchlist.matches.slice(0,1).map( x => {
                    
                     var matchPromise = new Promise(function(resolve, reject) {
                         kayn.Match.get(x['gameId']).then(game => {
@@ -32,11 +33,13 @@ router.route('/:name').get((req, res) => {
                 Promise.all(matchPromises).then(games => {
                     games.forEach(game =>{
 
-                        var id = (game['participantIdentities'].filter(x => x['player']['summonerName'].toUpperCase()==req.params.name.toUpperCase()))[0]['participantId'];
+                        //var id = (game['participantIdentities'].filter(x => x['player']['summonerName'].toUpperCase()==req.params.name.toUpperCase()))[0]['participantId'];
+                        //console.log(game)
+                        let id = jsonQuery('participantIdentities.player[summonerName='+req.params.name+']', { data: game});
                         var player = game['participants'].filter(x => x['participantId'] == id)[0];
+                        console.log(id);
                         var teamID = player['teamId'];
 
-                        console.log(player)
                         var matchDetails = {
                             outcome: game['teams'].filter(x=> x['teamId']==teamID)[0]['win'],
                             duration: game['gameDuration'],
@@ -77,7 +80,63 @@ router.route('/:name').get((req, res) => {
     })
     .catch(error => console.error(error))
  
+});*/
+
+function gameThing(game, name) {
+
+    var id = (game['participantIdentities'].filter(x => x['player']['summonerName'].toUpperCase()==name.toUpperCase()))[0]['participantId'];
+    var player = game['participants'].filter(x => x['participantId'] == id)[0];
+    var teamID = player['teamId'];
+
+    console.log(game);
+
+    return {
+        gameID: game.gameId,
+        outcome: game.teams.filter(x=> x['teamId']==teamID)[0]['win'],
+        duration: game.gameDuration,
+        summonerSpells : {
+            "spell1": jsonQuery('data[**][key='+player.spell1Id+']', { data: summoners}).value.name,
+            "spell2": jsonQuery('data[**][key='+player.spell2Id+']', { data: summoners}).value.name
+        },
+        KDA : player.stats.kills + "/" + player.stats.deaths + "/" + player.stats.assists,
+        championName: jsonQuery('data[**][key='+player.championId+']', { data: champs}).value.name,
+        maxLevel: player.stats.champLevel,
+        totalCS:  player.stats.totalMinionsKilled + player.stats.neutralMinionsKilled,
+        items:{
+            item0: player.stats.item0 ? items.data[player.stats.item0].name : "empty",
+            item1: player.stats.item1 ? items.data[player.stats.item1].name : "empty",
+            item2: player.stats.item2 ? items.data[player.stats.item2].name : "empty",
+            item3: player.stats.item3 ? items.data[player.stats.item3].name : "empty",
+            item4: player.stats.item4 ? items.data[player.stats.item4].name : "empty",
+            item5: player.stats.item5 ? items.data[player.stats.item5].name : "empty",
+            item6: player.stats.item6 ? items.data[player.stats.item6].name : "empty"
+        },
+        perks : {
+            perk0: jsonQuery('slots.runes[id='+player.stats.perk0+']', { data: runes}).value.key,
+            perk1: jsonQuery('slots.runes[id='+player.stats.perk1+']', { data: runes}).value.key,
+            perk2: jsonQuery('slots.runes[id='+player.stats.perk2+']', { data: runes}).value.key,
+            perk3: jsonQuery('slots.runes[id='+player.stats.perk3+']', { data: runes}).value.key,
+            perk4: jsonQuery('slots.runes[id='+player.stats.perk4+']', { data: runes}).value.key,
+            perk5: jsonQuery('slots.runes[id='+player.stats.perk5+']', { data: runes}).value.key
+        }
+    };
+}
+
+router.get('/:name', (req, res) => {
+    return kayn.Summoner.by.name(req.params.name)
+      .then(summoner => kayn.Matchlist.by.accountID(summoner['accountId']).region(REGIONS.NORTH_AMERICA))
+      .then(matchlist=> Promise.all(matchlist.matches.slice(0,1).map(match => kayn.Match.get(match.gameId))))
+      .then(games => games.map(game => gameThing(game, req.params.name)))
+      .then((matchesReturns) => res.status(200).json(matchesReturns))
+      .catch(error => {
+            console.log(error.error)
+            res.status(300).json(error.error)
+
+        })
+  
 });
+
+
 
 
 module.exports = router;
